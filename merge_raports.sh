4a49075722703dca9ceafb531c749906c305a471
Nemczek <p.baran890@gmail.com>
2024-05-16 18:42:48 +0200

    Added merge_raports

---
+++ merge_raports.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+checker=1
+
+# Check if there are raports in fastqc_run folder
+if ls "fastqc_run"/*.html 1> /dev/null 2>&1; then
+    $checker=0
+fi
+
+# Run multiqc
+if [ $checker = 0 ]; then
+    
+    if  [ ! -d "multiqc_run" ]; then
+        mkdir multiqc_run
+    fi
+
+    if ls "multiqc_run"/* 1> /dev/null 2>&1; then
+    echo "Do you want to delete all files in directory? (y/n)"
+    read user_answer
+
+    if [ $user_answer = "y" ]; then
+        rm multiqc_run/*
+    fi
+
+    multiqc --outdir multiqc_run  --title ngs_project
+fi
