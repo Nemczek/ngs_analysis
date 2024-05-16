d6cc9871eb551a687f079a6f4048d7d06f39d898
Nemczek <p.baran890@gmail.com>
2024-05-16 18:23:04 +0200

    Run fastqc script

---
+++ run_fastqc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if folder "fastqc_run" doesn't exists. 
+if  [ ! -d "fastqc_run" ]; then
+    mkdir fastqc_run
+fi
+
+# If there are any files in folder, ask if user wants to delete them
+if ls "fastqc_run"/*.fastqc 1> /dev/null 2>&1; then
+    echo "Do you want to delete all files in directory? (y/n)"
+    read user_answer
+
+    if [ $user_answer = "y" ]; then
+        rm fastqc_run/*.fastqc
+    fi
+fi
+
+# Run fastqc
+fastqc /fastqc_run/*.fastqc
+
+
