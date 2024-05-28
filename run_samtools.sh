1c7aa232a4f4bd60d01edb2868d40b45c74734f7
Virlhal <73465620+Virlhal@users.noreply.github.com>
2024-05-28 13:57:19 +0200

    samtools script

--- run_samtools.sh
+++ run_samtools.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+checker=1
 
-if ls "alignment_data"/*.sam 1> /dev/null 2>&1; then
+if ls "bam_data"/*.bam 1> /dev/null 2>&1; then
     $checker=0
 fi
 
-if  [ ! -d "bam_data" ]; then
-    mkdir bam_data
+if  [ ! -d "sorted_data" ]; then
+    mkdir sorted_data
+fi
+
+
+# Run samtools
+if [ $checker = 0 ]; then
+    
+    samtools sort -o bam_data/*.sorted.bam sorted_data/*.bam
+    samtools idenx bam_data/*.sorted.bam
+    
 fi
