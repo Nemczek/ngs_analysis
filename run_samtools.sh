84c7a9409aacfdb8ba4cb4036c7a8ad4b2a2fb64
Virlhal <73465620+Virlhal@users.noreply.github.com>
2024-05-28 19:02:32 +0200

    debuging

--- run_samtools.sh
+++ run_samtools.sh
@@ -10,10 +10,32 @@ if  [ ! -d "sorted_data" ]; then
 fi
 
 
+
+files=()
+
+
+for file in bam_data/*.bam; do
+    if [ -e "$file" ]; then
+        files+=("$file")
+    fi
+done
+
+# Debug: print parsed files
+echo "files: ${files[@]}"
+
+# Align files
+for index in "${!files[@]}"; do
+    file=${files[$index]} 
+    # Extract base names without extensions
+    base=$(basename "$file" .bam)
+    # Debug: print file names and base names
+    echo "Processing file: $file"
+    echo "Base name: $base"
 # Run samtools
-if [ $checker = 0 ]; then
+	if [ $checker = 0 ]; then
     
-    samtools sort -o ./bam_data/*.sorted.bam sorted_data/*.bam
-    samtools index ./bam_data/*.sorted.bam
+    		samtools sort "$file" -o ./sorted_data/${base}.sorted.bam
+    		samtools index ./sorted_data/${base}.sorted.bam
     
-fi
+	fi
+done
