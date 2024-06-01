b11bc1c7c5522a9b6796518e017d3c47354b0174
Nemczek <p.baran890@gmail.com>
2024-06-01 15:07:29 +0200

    added bcfrools run

---
+++ run_bcftools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Check if there is vcf file
+if ls "freebayes_run"/*.vcf &>/dev/null; then
+    # If true, run bcf tools to variants with quality > 20
+    bcftools filter -i 'QUAL > 20' freebayes_output.vcf -o ./freebayes_run/variants_filtered.vcf 
+fi
