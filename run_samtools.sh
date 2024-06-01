4f7bfc2b320b8a3c8261a8b7d31b3d73a8ea406e
Virlhal <73465620+Virlhal@users.noreply.github.com>
2024-06-01 20:10:14 +0200

    modified

--- run_samtools.sh
+++ run_samtools.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-checker=1
-
-if ls "bam_data"/*.bam 1> /dev/null 2>&1; then
-    checker=0
-fi
 
 if  [ ! -d "sorted_data" ]; then
     mkdir sorted_data
@@ -32,10 +27,9 @@ for index in "${!files[@]}"; do
     echo "Processing file: $file"
     echo "Base name: $base"
 # Run samtools
-	if [ $checker = 0 ]; then
+	
+   	samtools sort "$file" -o ./sorted_data/${base}.sorted.bam
+    samtools index ./sorted_data/${base}.sorted.bam
     
-    		samtools sort "$file" -o ./sorted_data/${base}.sorted.bam
-    		samtools index ./sorted_data/${base}.sorted.bam
-    
-	fi
+
 done
