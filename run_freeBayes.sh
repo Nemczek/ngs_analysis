5bb69ab7f91ab291b1eb6a9cbc69734b8852836a
Nemczek <p.baran890@gmail.com>
2024-06-01 19:27:44 +0200

    quick change

--- run_freeBayes.sh
+++ run_freeBayes.sh
@@ -2,9 +2,8 @@
 
 # Check if folder "freebayes_run" doesn't exist, create it if it does
 mkdir -p freebayes_run
-ls sorted_data/*.bam > freebayes_run/freebayes_input.txt
-# Check if there are any BAM files in the sorted_data folder
-if ls "sorted_data"/*.bam &>/dev/null; then
-    # If there are BAM files, run freebayes
-    freebayes -f ./data/ref.fa -L freebayes_run/freebayes_input.txt > freebayes_run/freebayes_output.vcf
-fi
+
+for BAM in sorted_data/*sorted.bam; do
+    SAMPLE_NAME=$(basename $BAM _sorted.bam)
+    freebayes -f data/ref.fa -q 30 -b $BAM > freebayes_run/${SAMPLE_NAME}.vcf
+done
