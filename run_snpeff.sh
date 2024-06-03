ce694af72a3cd98dc618f274b341f0880cc96cd2
Virlhal <73465620+Virlhal@users.noreply.github.com>
2024-06-03 18:45:37 +0200

    debug

--- run_snpeff.sh
+++ run_snpeff.sh
@@ -9,7 +9,7 @@ if [ $user_answer = "y" ]; then
     if ls "bfctools_run"/bfctools_vf.vcf &>/dev/null; then
         # Annotate
         snpEff -v Penicillium_chrysogenum ./bfctools_run/bfctools_vf.vcf > bfc_annotated.vcf
-        mv bfc_annotated ./data
+        mv bfc_annotated.vcf ./data
     fi
     else
     echo "Download database manually or use snpEff database and check for your organism."
